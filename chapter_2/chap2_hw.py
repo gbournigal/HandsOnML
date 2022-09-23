bd7dc079d2e6d0890d90528cc9b3d1c850b6690d
gbournigal <georgesbournigal@gmail.com>
2022-09-23 17:35:30 -0400

    chapter 2 ready
    Lo unico pendiente que me gustaría lograr es poder sacar los nombres de las variables seleccionadas

--- chapter_2/chap2_hw.py
+++ chapter_2/chap2_hw.py
@@ -8,12 +8,15 @@ Created on Wed Sep 21 10:43:33 2022
 import pandas as pd
 import numpy as np
 from utils import load_housing_data, HOUSING_PATH, full_pipeline, num_pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV, RandomizedSearchCV
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler, PowerTransformer
 from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
 from scipy.stats import loguniform
+from sklearn.feature_selection import SelectFromModel
 
 
 def get_housing_prepared(full_pipeline):
@@ -25,6 +28,8 @@ def get_housing_prepared(full_pipeline):
     for train_index, test_index in split.split(housing, housing["income_cat"]):
         strat_train_set = housing.loc[train_index]
         strat_test_set = housing.loc[test_index]
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
         
     housing = strat_train_set.drop("median_house_value", axis=1) # drop labels for training set
     housing_labels = strat_train_set["median_house_value"].copy()
@@ -33,16 +38,17 @@ def get_housing_prepared(full_pipeline):
     cat_attribs = ["ocean_proximity"]
     
     full_pipeline = full_pipeline(num_pipeline, 
-                                  OneHotEncoder(), 
+                                  OneHotEncoder(handle_unknown="ignore"), 
                                   num_attribs,
                                   cat_attribs)
     
     housing_prepared = full_pipeline.fit_transform(housing)
     
-    return housing_prepared, housing_labels, strat_test_set, full_pipeline
+    return housing, housing_prepared, housing_labels, strat_test_set, full_pipeline
 
 
-housing_prepared, housing_labels, strat_test_set, full_pipeline = get_housing_prepared(full_pipeline)
+housing, housing_prepared, housing_labels, strat_test_set, full_pipeline = get_housing_prepared(full_pipeline)
+
 
 def exercise_1():
     param_grid = [
@@ -83,20 +89,92 @@ def exercise_2():
     rnd_search.fit(housing_prepared, housing_labels)
     return rnd_search
 
+# class TopFeatureSelector(BaseEstimator, TransformerMixin):
+#     def __init__(self, feature_importances, k):
+#         self.feature_importances = feature_importances
+#         self.k = k
+#     def fit(self, X, y=None):
+#         self.feature_indices_ = np.argsort(self.feature_importances)[-self.k:]
+#         print(self.feature_indices_)
+#         return self
+#     def transform(self, X):
+#         print(X.shape)
+#         return X[:, self.feature_indices_]
 
 def exercise_3():
     forest_reg = RandomForestRegressor(n_estimators=180,
                                        max_features=7,
                                        random_state=42)
-    forest_reg.fit(housing_prepared, housing_labels)
-    feature_importances = forest_reg.feature_importances_
-    feature_importances
-    
-    num_attribs = full_pipeline.transformers[0][2]
-    extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
-    #cat_encoder = cat_pipeline.named_steps["cat_encoder"] # old solution
-    cat_encoder = full_pipeline.named_transformers_["cat"]
-    cat_one_hot_attribs = list(cat_encoder.categories_[0])
-    attributes = num_attribs + extra_attribs + cat_one_hot_attribs
     
     
+    preparation_selection_pipeline = Pipeline([
+        ('preparation', full_pipeline),
+        ('feature_selection', SelectFromModel(estimator=forest_reg,
+                                              threshold=0.005))
+    ])
+    
+    housing_prepared_top = preparation_selection_pipeline.fit_transform(housing, housing_labels)
+    
+    return preparation_selection_pipeline, housing_prepared_top, feature_importances
+
+
+preparation_selection_pipeline, _, feature_importances = exercise_3()
+
+
+def exercise_4():
+    rnd_search = exercise_2()
+
+    forest_reg = RandomForestRegressor(n_estimators=180,
+                                       max_features=7,
+                                       random_state=42)
+
+
+    prep_select_predict_pipeline = Pipeline(
+        [
+        ('preparation', full_pipeline),
+        ('feature_selection', SelectFromModel(estimator=forest_reg,
+                                              threshold=0.005)),
+        ('model', SVR(**rnd_search.best_params_))]
+        )
+    
+    prep_select_predict_pipeline.fit(housing, housing_labels)
+    return prep_select_predict_pipeline
+    
+prep_select_predict_pipeline = exercise_4()
+
+
+def exercise_5(prep_select_predict_pipeline):
+    prep_select_predict_pipeline.named_steps['preparation'].named_transformers_["cat"].handle_unknown = 'ignore'
+
+    param_grid = [{
+        'preparation__num__imputer__strategy': ['mean', 
+                                                # 'median', 
+                                                'most_frequent'],
+        'feature_selection__threshold': [0.0001,
+                                         0.001, 
+                                         0.005, 
+                                         0.01, 
+                                         0.02, 
+                                         0.05, 
+                                         0.1, 
+                                         0.2]
+    }]
+    
+    grid_search_prep = GridSearchCV(prep_select_predict_pipeline, 
+                                    param_grid,
+                                    cv=2,
+                                    scoring='neg_mean_squared_error', 
+                                    verbose=2,
+                                    error_score='raise',
+                                    n_jobs=-1
+                                    )
+    grid_search_prep.fit(housing, housing_labels)
+    
+    grid_search_prep.best_params_
+    negative_mse = grid_search_prep.best_score_
+    rmse = np.sqrt(-negative_mse)
+    print(rmse)
+    
+
+if __name__ == '__main__':
+    exercise_5(prep_select_predict_pipeline)
